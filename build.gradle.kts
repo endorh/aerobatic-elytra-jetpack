b53f9c8f26ea91613b0ef3ac59887118c452c51b
Endor H <86890810+endorh@users.noreply.github.com>
2022-09-05 21:23:03 +0200

    🚀 Add GitHub Packages repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ plugins {
 
 val modId = "aerobaticelytrajetpack"
 val modGroup = "endorh.aerobaticelytra.jetpack"
+val githubRepo = "endorh/aerobaticelytrajetpack"
 val modVersion = "0.2.20"
 val mcVersion = "1.18.2"
 val forge = "40.1.0"
@@ -366,6 +367,14 @@ artifacts {
 
 publishing {
 	repositories {
+		maven("https://maven.pkg.github.com/$githubRepo") {
+			name = "GitHubPackages"
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+				password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+			}
+		}
+		
 		maven(rootProject.projectDir.parentFile.resolve("maven")) {
 			name = "LocalMods"
 		}
